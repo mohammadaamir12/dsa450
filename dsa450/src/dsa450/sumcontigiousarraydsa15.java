8ab22b77db381553f5d9e870a6264b6c34a7f8fc
Md Aamir <asadahmadkh344@gmail.com>
2022-04-26 14:57:10 +0530

    practise

---
+++ dsa450/src/dsa450/sumcontigiousarraydsa15.java
@@ -0,0 +1,33 @@
+package dsa450;
+
+import java.util.Scanner;
+
+public class sumcontigiousarraydsa15 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the size of Array");
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		System.out.println("Enter the size of Array");
+		for(int i=0;i<n;i++){
+		    a[i]=sc.nextInt();
+		}
+		int max=Integer.MIN_VALUE;
+		int maxhere=0;
+		for(int i=0;i<n;i++){
+		    maxhere=maxhere+a[i];
+		    if(maxhere>max) {
+		    	max=maxhere;
+		    }
+		    if(maxhere<0) {
+		    	maxhere=0;
+		    }
+		}
+		
+			System.out.println(max);
+		
+
+	}
+
+}
