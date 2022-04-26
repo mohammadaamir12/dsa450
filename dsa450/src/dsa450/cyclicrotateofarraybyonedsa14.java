3e600483d0f1dd45e78bd769656fcebb9c9a42d8
Md Aamir <asadahmadkh344@gmail.com>
2022-04-26 14:38:52 +0530

    practise

---
+++ dsa450/src/dsa450/cyclicrotateofarraybyonedsa14.java
@@ -0,0 +1,27 @@
+package dsa450;
+
+import java.util.Scanner;
+
+public class cyclicrotateofarraybyonedsa14 {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the size of Array");
+		int n=sc.nextInt();
+		int a[]=new int[n];
+		System.out.println("Enter the size of Array");
+		for(int i=0;i<n;i++){
+		    a[i]=sc.nextInt();
+		}
+		int x=a.length;
+		for(int j=a.length-1;j>0;j--) {
+			a[j]=a[j-1];
+		}
+		a[0]=x;
+       
+		for(int i=0;i<n;i++){
+		   System.out.print(a[i]);
+		}
+	}
+
+}
