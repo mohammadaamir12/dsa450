a0c15d70482b15180d6431b4a2ff61f32135319f
Md Aamir <asadahmadkh344@gmail.com>
2022-07-02 23:00:36 +0530

    modify

--- dsa450/src/dsa450/Kadanealgodsa15.java
+++ dsa450/src/dsa450/Kadanealgodsa15.java
@@ -8,7 +8,7 @@ public static void main(String[] args) {
 			System.out.println("Enter the size of Array");
 			int n=sc.nextInt();
 			int a[]=new int[n];
-			System.out.println("Enter the size of Array");
+			System.out.println("Enter the element of Array");
 			for(int i=0;i<n;i++){
 			    a[i]=sc.nextInt();
 			}
