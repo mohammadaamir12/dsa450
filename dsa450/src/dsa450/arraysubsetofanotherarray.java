ec44bb39301b4babbc2441473f6a71de76586961
Md Aamir <asadahmadkh344@gmail.com>
2022-07-07 14:12:28 +0530

    dsa

---
+++ dsa450/src/dsa450/arraysubsetofanotherarray.java
@@ -0,0 +1,45 @@
+package dsa450;
+
+import java.util.Arrays;
+
+public class arraysubsetofanotherarray {
+
+	public static void main(String[] args) {
+		   int[]arr= {10,5,2,23,19};
+			int n1=arr.length;
+			int[]ar= {19,5,2};
+		    int n2=ar.length;
+		   count(arr,ar,n1,n2);
+	   if(count(arr,ar,n1,n2)==true) {
+		   System.out.println("a2 is subset ");
+	   }
+	   else {
+		   System.out.println("a2 not is subset "); 
+	   }
+		}
+	
+	private static boolean count(int[] arr, int[] ar, int n1, int n2) {
+		int temp=0;
+		int j=0;
+		int i=0;
+		for(i=0;i<n2;i++) {
+		for( j=0;j<n1;j++) {
+			if(ar[i]==arr[j]) {
+				break;
+			}
+			
+		}
+		if(j==n1) 
+			return false;
+		
+} 
+		return true;	
+	}
+	
+		
+	
+
+	
+	}
+
+
--- dsa450/src/dsa450/neww.java
+++ dsa450/src/dsa450/neww.java
@@ -4,19 +4,19 @@ import java.util.*;
 public class neww {
 
 	public static void main(String[] args) {
-		  int[][] arr = { {0,1,2,4,5}, {3,4,5,6,7},{5,6,7,8,9} }; 
-		    System.out.println(fun(arr)); 
-	}
-	public static int fun(int[][] arr2d)
-	{
-	    int sum=0;
-	    for(int i=0;i<arr2d.length;i++)
-	    {
-	        for(int j=0;j<arr2d[i].length;j++)
-	        {
-	            sum+=arr2d[i][j];
+		 Scanner sc=new Scanner(System.in);
+	        int n=sc.nextInt();
+	        int a[]=new int[n];
+	        for(int i=0;i<n;i++){
+	            a[i]=sc.nextInt();
 	        }
-	    }
-	    return sum;
+
+	         int sum=0;
+	         int j=0;
+	        for( j=0;j<n;j++){
+	           sum+=a[j];
+	        }
+	        
+	        System.out.print(sum);
 	}
 }
