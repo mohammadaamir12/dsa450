3228d961185fa71d31c14d846e2c1272acdd85cf
Md Aamir <asadahmadkh344@gmail.com>
2022-07-21 10:49:53 +0530

    dsa450

---
+++ dsa450/src/dsa450/missingnumber.java
@@ -0,0 +1,19 @@
+package dsa450;
+
+public class missingnumber {
+
+	public static void main(String[] args) {
+		int[]nums= {3,0,1,2};
+        int res=0;
+        int n=nums.length;
+        for(int i=0;i<=n;i++){
+           res=res^i; 
+        }
+        for(int j=0;j<nums.length;j++){
+            res=res^nums[j];
+        }
+System.out.println(res);
+
+	}
+
+}
