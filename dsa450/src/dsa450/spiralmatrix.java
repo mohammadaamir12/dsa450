0b1edd4384597136728cc3e3a81caa79c2a1d4f2
Md Aamir <asadahmadkh344@gmail.com>
2022-07-27 19:44:28 +0530

    dsa450

---
+++ dsa450/src/dsa450/spiralmatrix.java
@@ -0,0 +1,54 @@
+package dsa450;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class spiralmatrix {
+
+	public static void main(String[] args) {
+	
+		
+		 ArrayList<Integer> spiral = new ArrayList<Integer>();
+	       int startrow=0;
+	       int startcol=0;
+	       int endrow = r-1;
+	       int endcol=c-1;
+	       while((startrow<=endrow) && (startcol<=endcol))
+	       {
+	           for(int i=startcol;i<=endcol;i++)
+	           {
+	               spiral.add(matrix[startrow][i]);
+	           }
+	           startrow++;
+	           if(startrow<=endrow && startcol<=endcol)
+	           {
+	               for(int i=startrow;i<=endrow;i++)
+	               {
+	                   spiral.add(matrix[i][endcol]);
+	               }
+	           }
+	           endcol--;
+	           if(startrow<=endrow && startcol<=endcol)
+	           {
+	               for(int i=endcol;i>=startcol;i--)
+	               {
+	                   spiral.add(matrix[endrow][i]);
+	               }
+	           }
+	          endrow--;
+	           if(startrow<=endrow && startcol<=endcol)
+	           {
+	               for(int i=endrow;i>=startrow;i--)
+	               {
+	                   spiral.add(matrix[i][startcol]);
+	               }
+	           }
+	           startcol++;
+	       }
+	       return spiral;
+	       
+
+	}
+
+}
